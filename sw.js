13a38cb8565d9173e7c19e86530cbdff880c89ea
WaveSimm <yunsim@gmail.com>
2026-08-11 07:46:35 +0900

    style: 장 번호 본문 크기로, 절 번호 청색 계열로 가독성 개선

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // BibleNote Service Worker — 앱 셸 + 성경 데이터 오프라인 캐시
-const VER = "biblenote-v5";
+const VER = "biblenote-v6";
 const SHELL = [
   "./", "index.html", "css/app.css",
   "js/main.js", "js/data.js", "js/parser.js", "js/store.js",
